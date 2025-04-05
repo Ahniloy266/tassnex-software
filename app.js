b3cc5c9135e7d011043f1b15b48d0c70a6c40a24
Ahniloy266 <niloyhaque21@gmail.com>
2025-04-05 17:02:11 +0600

    Add files via upload
    new final

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-// Wait until DOM is fully loaded
+
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
-
   form.addEventListener("submit", function (e) {
-    e.preventDefault(); // Prevent form from refreshing the page
-
+    e.preventDefault();
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    // Check credentials
     if (username === "admin" && password === "1234") {
-      // Redirect to dashboard
       window.location.href = "dashboard.html";
     } else {
       alert("Invalid username or password");
     }
   });
-});console.log('Tassnex app running');
+});
